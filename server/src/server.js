747f9ca8d72c2129ad332449a333be47c4f35b52
Natnael <natnaeltilahun157@gmail.com>
2025-06-17 16:38:54 +0200

    fix cors

--- server/src/server.js
+++ server/src/server.js
@@ -21,7 +21,7 @@ const server = http.createServer(app);
 
 // Configure CORS
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: "*",
   methods: ["GET", "POST"],
   credentials: true,
 };
